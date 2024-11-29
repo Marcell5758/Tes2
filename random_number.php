11f1ccee8967a41d473469d1e291357478126988
Marcell <situasi3@gmail.com>
2024-11-29 10:52:24 +0700

    Tambah file

---
+++ random_number.php
@@ -0,0 +1,18 @@
+<?php
+// Fungsi untuk menghasilkan angka acak
+$randomNumber = rand(1, 100); // Angka acak antara 1 dan 100
+
+// Tampilkan hasilnya
+echo "Angka acak yang dihasilkan: " . $randomNumber;
+
+/*
+    Paragraf berikut adalah penjelasan tambahan:
+    Kode ini menggunakan fungsi `rand()` bawaan PHP untuk menghasilkan angka
+    acak dalam rentang tertentu. Angka tersebut ditampilkan ke layar dengan
+    fungsi `echo`. Anda dapat mengganti rentang angka pada fungsi `rand()`
+    sesuai kebutuhan. Kode ini menggunakan fungsi `rand()` bawaan PHP untuk menghasilkan angka
+    acak dalam rentang tertentu. Angka tersebut ditampilkan ke layar dengan
+    fungsi `echo`. Anda dapat mengganti rentang angka pada fungsi `rand()`
+    sesuai kebutuhan.
+*/
+?>
